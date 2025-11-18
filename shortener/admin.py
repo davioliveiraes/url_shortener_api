32bb12ae973c5aaa5e6a91ed662eb6a1170742ca
Davi Oliveira <davioliveiraes7@gmail.com>
2025-11-18 11:38:29 -0300

    feat: create database models for URL shortener

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,1 +1,76 @@
 from django.contrib import admin
+from django.http import HttpRequest
+
+from .models import Click, ShortenedURL
+
+
+@admin.register(ShortenedURL)
+class ShortenedURLAdmin(admin.ModelAdmin):
+    list_display = [
+        "short_code",
+        "original_url_truncated",
+        "is_active",
+        "total_clicks",
+        "expires_at",
+        "created_at",
+    ]
+
+    list_filter = ["is_active", "created_at", "expires_at"]
+    search_fields = ["short_code", "original_url"]
+    readonly_fields = [
+        "short_code",
+        "total_clicks",
+        "unique_clicks",
+        "qr_code",
+        "created_at",
+        "updated_at",
+    ]
+    date_hierarchy = "created_at"
+
+    fieldsets = (
+        (
+            "Informacoes Basicas",
+            {"fields": ("original_url", "short_code", "qr_code")},
+        ),
+        (
+            "Configuracoes",
+            {
+                "fields": ("is_active", "expires_at", "max_clicks"),
+                "description": "Configure as restricoes do link",
+            },
+        ),
+        (
+            "Estatisticas",
+            {
+                "fields": ("total_clicks", "unique_clicks"),
+                "description": "Contadores de cliques",
+            },
+        ),
+        (
+            "Metadados",
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+                "description": "Informacoes de criacao e atualizacao",
+            },
+        ),
+    )
+
+    def original_url_truncated(self, obj):
+        if len(obj.original_url) > 50:
+            return obj.original_url[:47] + "..."
+        return obj.original_url
+
+    original_url_truncated.short_description = "URL Original"
+
+
+@admin.register(Click)
+class ClickAdmin(admin.ModelAdmin):
+    list_display = ["url", "ip_address", "clicked_at"]
+    list_filter = ["clicked_at"]
+    search_fields = ["url_short_code", "ip_address"]
+    readonly_fields = ["url", "ip_address", "user_agent", "referer", "clicked_at"]
+    date_hierarchy = "clicked_at"
+
+    def has_add_permission(self, request):
+        return False
---
+++ shortener/models.py
@@ -0,0 +1,146 @@
+from django.db import models
+from django.utils import timezone
+
+
+class ShortenedURL(models.Model):
+    original_url = models.URLField(
+        verbose_name="URL Original",
+        max_length=2048,
+        help_text="URL completa que sera encurtada",
+    )
+
+    short_code = models.CharField(
+        verbose_name="Codigo Curto",
+        max_length=10,
+        unique=True,
+        db_index=True,
+        help_text="Codigo unico para a URL encurtada",
+    )
+
+    is_active = models.BooleanField(
+        default=True, verbose_name="Ativo", help_text="Define se o link esta ativo"
+    )
+
+    expires_at = models.DateTimeField(
+        null=True,
+        blank=True,
+        verbose_name="Expira em",
+        help_text="Data e hora de expiracao do link",
+    )
+
+    max_clicks = models.PositiveIntegerField(
+        default=0,
+        verbose_name="Maximo de Cliques unicos",
+        help_text="Numero maximo de cliques unicos permitidos",
+    )
+
+    total_clicks = models.PositiveIntegerField(
+        default=0,
+        verbose_name="Total de Cliques",
+        help_text="Contador total de cliques",
+    )
+
+    unique_clicks = models.PositiveIntegerField(
+        default=0,
+        verbose_name="Cliques Unicos",
+        help_text="Contador de cliques unicos (baseadoem IP)",
+    )
+
+    qr_code = models.ImageField(
+        upload_to="qrcodes/",
+        null=True,
+        blank=True,
+        verbose_name="QR Code",
+        help_text="Imagem do QR Code gerado automaticamente",
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Criado em", help_text="Data de criacao"
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Atualizado em", help_text="Data da utlima atualizacao"
+    )
+
+    class Meta:
+        verbose_name = "URL Encurtada"
+        verbose_name_plural = "URLs Encurtadas"
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["short_code"]),
+            models.Index(fields=["created_at"]),
+            models.Index(fields=["is_active"]),
+        ]
+
+    def __str__(self):
+        return f"{self.short_code} -> {self.original_url}"
+
+    def is_expired(self):
+        if self.expires_at and timezone.now() > self.expires_at:
+            return True
+        return False
+
+    def has_reached_max_clicks(self):
+        if self.max_clicks and self.unique_clicks >= self.max_clicks:
+            return True
+        return False
+
+    def can_be_accessed(self):
+        if not self.is_active:
+            return False, "Link desativado"
+        if self.is_expired():
+            return False, "Link desativado"
+        if self.has_reached_max_clicks():
+            return False, "Limite de cliques atingido"
+        return True, "OK"
+
+    def increment_clicks(self, is_unique=False):
+        self.total_clicks += 1
+        if is_unique:
+            self.unique_clicks += 1
+        self.save(update_fields=["total_clicks", "unique_clicks"])
+
+
+class Click(models.Model):
+    url = models.ForeignKey(
+        ShortenedURL,
+        on_delete=models.CASCADE,
+        related_name="Clicks",
+        verbose_name="URL",
+        help_text="URL encurtada que foi clicada",
+    )
+
+    ip_address = models.GenericIPAddressField(
+        verbose_name="Endereco IP", help_text="IP do usuario que clicou"
+    )
+
+    user_agent = models.TextField(
+        blank=True, verbose_name="User Agent", help_text="Navegador e sistema operacional"
+    )
+
+    referer = models.URLField(
+        blank=True,
+        null=True,
+        max_length=2048,
+        verbose_name="Referencia",
+        help_text="URL de onde veio o clique",
+    )
+
+    clicked_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Clicando em",
+        help_text="Data e hora do clique",
+    )
+
+    class Meta:
+        verbose_name = ("Clique",)
+        verbose_name_plural = "Cliques"
+        ordering = ["-clicked_at"]
+        indexes = [
+            models.Index(fields=["url", "ip_address"]),
+            models.Index(fields=["clicked_at"]),
+            models.Index(fields=["url", "clicked_at"]),
+        ]
+
+    def __str__(self):
+        return f"Clique em {self.url.short_code} - {self.clicked_at}"
